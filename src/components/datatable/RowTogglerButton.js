de5b88db34c5a15cb49f1a99f3dd48e062667a0e
Ali Erenkara <alierenkara@gmail.com>
2020-10-14 12:08:36 +0300

    formatted datatable codes

--- src/components/datatable/RowTogglerButton.js
+++ src/components/datatable/RowTogglerButton.js
@@ -1,45 +1,50 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { Ripple } from '../ripple/Ripple';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { Ripple } from "../ripple/Ripple";
 
 export class RowTogglerButton extends Component {
+  static defaultProps = {
+    rowData: null,
+    onClick: null,
+    expanded: false,
+  };
 
-    static defaultProps = {
-        rowData: null,
-        onClick: null,
-        expanded: false
+  static propTypes = {
+    rowData: PropTypes.object,
+    onClick: PropTypes.func,
+    expanded: PropTypes.bool,
+  };
+
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(event) {
+    if (this.props.onClick) {
+      this.props.onClick({
+        originalEvent: event,
+        data: this.props.rowData,
+      });
     }
+  }
 
-    static propTypes = {
-        rowData: PropTypes.object,
-        onClick: PropTypes.func,
-        expanded: PropTypes.bool
-    }
+  render() {
+    let iconClassName = classNames("p-row-toggler-icon pi pi-fw p-clickable", {
+      "pi-chevron-down": this.props.expanded,
+      "pi-chevron-right": !this.props.expanded,
+    });
 
-    constructor(props) {
-        super(props);
-        this.onClick = this.onClick.bind(this);
-    }
-
-    onClick(event) {
-        if(this.props.onClick) {
-            this.props.onClick({
-                originalEvent: event,
-                data: this.props.rowData
-            })
-        }
-    }
-
-    render() {
-        let iconClassName = classNames('p-row-toggler-icon pi pi-fw p-clickable', {
-            'pi-chevron-down': this.props.expanded,
-            'pi-chevron-right': !this.props.expanded
-        });
-
-        return  <button type="button" onClick={this.onClick} className="p-row-toggler p-link">
-                    <span className={iconClassName}></span>
-                    <Ripple />
-                </button>
-    }
+    return (
+      <button
+        type="button"
+        onClick={this.onClick}
+        className="p-row-toggler p-link"
+      >
+        <span className={iconClassName}></span>
+        <Ripple />
+      </button>
+    );
+  }
 }
